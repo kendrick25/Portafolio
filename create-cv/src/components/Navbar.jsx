7541c1e85c64b0c32851ef137545858112feaccd
kendrick25 <kendricks715@gmail.com>
2025-03-28 21:46:19 -0500

    Update Navbar.jsx
    Logo name

--- create-cv/src/components/Navbar.jsx
+++ create-cv/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
-           Mail<span className="text-blue-500">To</span><span className="text-green-500">.Code</span>
+          <span className="text-green-500">{"</> "}</span>Mail<span className="text-blue-500">To</span><span className="text-green-500">.Code</span>
           </a>
 
           {/* Ocultar la barra cuando el menú está abierto */}
