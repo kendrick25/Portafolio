4b2fd2e574b9645837f75d8f32fe560352cd9036
kendrick25 <kendricks715@gmail.com>
2025-04-03 16:19:11 -0500

    Projects
    frontend del los proyectos, scrol añadido y creacion de la section Contact

--- create-cv/src/App.jsx
+++ create-cv/src/App.jsx
@@ -6,6 +6,8 @@ import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
 import { Home } from './components/sections/home';
 import { About } from './components/sections/About';
+import { Projects } from './components/sections/Projects';
+import { Contact } from './components/sections/Contact';
 
 function App() {
   const [isLoaded, setIsLoaded]= useState(false);
@@ -22,7 +24,9 @@ function App() {
         <MobileMenu  menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <Home />
         <About />
-
+        <Projects />
+        <Contact />
+        
     </div>
     </>
 
